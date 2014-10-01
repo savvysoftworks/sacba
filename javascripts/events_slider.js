00fe76911a12c3a8e2e0826a756249835be7976b
Savannah Butler <savannahscarrone@gmail.com>
2014-10-01 10:16:19 -0400

    "Automated commit at 2014-10-01 14:16:19 UTC by middleman-deploy 0.1.4"

---
+++ javascripts/events_slider.js
@@ -0,0 +1,11 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 425,
+    itemMargin: 20,
+    minItems: 2,
+    maxItems: 2,
+    animationLoop: true
+  });
+});
